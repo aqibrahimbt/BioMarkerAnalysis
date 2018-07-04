afb25415d3dff019d44587ed86334c68973c02ab
Markus Fricke <fricke@genevention.com>
2018-07-04 10:55:38 +0200

    add first test

--- gToolbox/tests/testthat/test-de_analysis.R
+++ gToolbox/tests/testthat/test-de_analysis.R
@@ -1,5 +1,11 @@
 context("test-de_analysis.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("get valid comparisions out of conditions", {
+  conditions = readRDS(file = "conditions.rds")
+  #saveRDS(dataset_data$conditions, file = "./gToolbox/tests/testthat/conditions.rds")
+  comparisons = get_comparisons(conditions)
+  comparisons_ref =  readRDS(file = "comparisons.rds")
+  #saveRDS(comparisons, file = "./gToolbox/tests/testthat/comparisons.rds")
+  expect_identical(comparisons$conditions,comparisons_ref$conditions)
+  expect_identical(comparisons$samples,comparisons_ref$samples)
 })
